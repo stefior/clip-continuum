cc4b8cc185bb5b37921b9913f1815dbbd718f7d2
Stefior <stephencfiorenza@gmail.com>
2023-05-20 19:22:13 -0400

    Set logo and logo text to link home

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav id="navbar" className="flex items-center">
-      <img src="./logo.png" alt="Code Continuum logo" className="h-12 select-none" />
-      <h1 className="text-2xl font-title select-none">Code Continuum</h1>
+      <a href="/" className="flex items-center">
+        <img src="./logo.png" alt="Code Continuum logo" className="h-12 select-none" />
+        <h1 className="text-2xl font-title select-none">Code Continuum</h1>
+      </a>
       <div className="ml-auto p-4 font-title text-sm">
         <Link to="/tutorial" className="ml-4 hover:underline">
           Tutorial
